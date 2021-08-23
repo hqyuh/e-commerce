7b857d48bfc87246718a24625296a3f38e2874d4
OS <hoquanghuy0123@gmail.com>
2021-08-23 14:43:18 +0700

    test paging

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -7,12 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@DataJpaTest(showSql = false)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 class UserRepositoryTest {
@@ -132,5 +137,22 @@ class UserRepositoryTest {
         repo.updateEnabledStatus(id, true);
     }
 
+
+    @Test
+    public void testListFirstPage(){
+
+        int pageNumber = 0;
+        int pageSize = 4;
+
+        Pageable pageable = PageRequest.of(pageNumber,pageSize );
+        Page<User> page =  repo.findAll(pageable);
+
+        List<User> listUsers = page.getContent();
+
+        listUsers.forEach(System.out::println);
+
+        assertThat(listUsers.size()).isEqualTo(pageSize);
+
+    }
 
 }
