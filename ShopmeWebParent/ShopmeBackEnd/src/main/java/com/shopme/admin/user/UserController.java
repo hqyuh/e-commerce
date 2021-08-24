fe3fd8fa9de4f4c02c464aaafcd2da8e7c4bf526
OS <hoquanghuy0123@gmail.com>
2021-08-24 15:00:28 +0700

    test sort

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -35,16 +35,20 @@ public class UserController {
         // model.addAttribute("listUsers", listUsers);
         // return "users";
 
-        return listByPage(1, model);
+        return listByPage(1, model, "firstName", "asc");
 
     }
 
 
     @GetMapping("/users/page/{pageNum}")
     public String listByPage(@PathVariable(name = "pageNum") int pageNum,
-                             Model model){
+                             Model model,
+                             String sortField,
+                             String sortDir){
 
-        Page<User> page = service.listByPage(pageNum);
+        System.out.println(sortField + " " + sortDir);
+
+        Page<User> page = service.listByPage(pageNum, sortField, sortDir);
         List<User> listUsers = page.getContent();
 
         long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
