688e3d7d502f37cf72231c88a5208532c976415b
OS <hoquanghuy0123@gmail.com>
2021-08-26 22:36:15 +0700

    completed export excel

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserExcelExporter.java
@@ -1,10 +1,8 @@
 package com.shopme.admin.user;
 
 import com.shopme.common.entity.User;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.*;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
@@ -13,24 +11,97 @@ import java.util.List;
 
 public class UserExcelExporter extends AbstractExporter{
 
+    // create workbook
+    private XSSFWorkbook workbook;
+
+    // create sheet
+    private XSSFSheet sheet;
+
+    public UserExcelExporter() {
+        workbook = new XSSFWorkbook();
+    }
+
+    private void writeHeaderLine(){
+        sheet = workbook.createSheet("Users");
+        XSSFRow row = sheet.createRow(0);
+
+        // create font
+        XSSFCellStyle cellStyle = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight(16);
+        cellStyle.setFont(font);
+        //
+
+
+        createCell(row, 0, "User ID", cellStyle);
+        createCell(row, 1, "Email", cellStyle);
+        createCell(row, 2, "First Name", cellStyle);
+        createCell(row, 3, "Last Name", cellStyle);
+        createCell(row, 4, "Roles", cellStyle);
+        createCell(row, 5, "Enabled", cellStyle);
+
+    }
+
+    private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style){
+
+        //
+        XSSFCell cell = row.createCell(columnIndex);
+
+        // spacing
+        sheet.autoSizeColumn(columnIndex);
+
+        if(value instanceof Integer){
+            cell.setCellValue((Integer)value);
+        }else if(value instanceof  Boolean){
+            cell.setCellValue((Boolean) value);
+        }else {
+            cell.setCellValue((String) value);
+        }
+
+        cell.setCellStyle(style);
+    }
+
     public void export(List<User> listUsers,
                        HttpServletResponse response) throws IOException {
 
         super.setResponseHeader(response, "application/octet-stream", ".xlsx");
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("Users");
-
-
-        XSSFRow row = sheet.createRow(0);
-        XSSFCell cell = row.createCell(0);
-        cell.setCellValue("User ID");
+        writeHeaderLine();
+        writeDataLine(listUsers);
 
         ServletOutputStream outputStream = response.getOutputStream();
         workbook.write(outputStream);
-
         workbook.close();
         outputStream.close();
+
+    }
+
+    private void writeDataLine(List<User> listUsers) {
+
+        int rowIndex = 1;
+
+        // create font
+        XSSFCellStyle cellStyle = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setFontHeight(14);
+        cellStyle.setFont(font);
+        //
+
+        for (User user: listUsers) {
+
+            // row: hàng ngang
+            XSSFRow row = sheet.createRow(rowIndex++);
+
+            int columnIndex = 0;
+
+            createCell(row, columnIndex++, user.getId(), cellStyle);
+            createCell(row, columnIndex++, user.getEmail(), cellStyle);
+            createCell(row, columnIndex++, user.getFirstName(), cellStyle);
+            createCell(row, columnIndex++, user.getLastName(), cellStyle);
+            createCell(row, columnIndex++, user.getRoles().toString(), cellStyle);
+            createCell(row, columnIndex++, user.isEnabled(), cellStyle);
+        }
 
     }
 
