3b2d727b5cdbcb03104ef6460ec7c876b7dea8c9
OS <hoquanghuy0123@gmail.com>
2021-08-30 21:30:21 +0700

    show user's full name

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -63,4 +63,9 @@ public class ShopmeUserDetails implements UserDetails {
     public boolean isEnabled() {
         return user.isEnabled();
     }
+
+    public String getFullName(){
+        return this.user.getFirstName() + " " + this.user.getLastName();
+    }
+
 }
