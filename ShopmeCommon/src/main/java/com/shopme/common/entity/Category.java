c25024c732b601d74140e2d217bd297ddf2c1f0d
OS <hoquanghuy0123@gmail.com>
2021-09-02 21:08:53 +0700

    code category entity/repository

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -0,0 +1,43 @@
+package com.shopme.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private String alias;
+
+    @Column(length = 64, nullable = false, unique = true)
+    private String image;
+
+    @Column(length = 128, nullable = false, unique = true)
+    private boolean enabled;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> children = new HashSet<>();
+
+}
